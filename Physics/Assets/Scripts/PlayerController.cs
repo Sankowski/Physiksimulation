3823fdc50000e9bea085c3a944f9f6e260e6c394
Sankowski <thomas-sankowski@t-online.de>
2018-04-10 13:11:34 +0200

    add: addforce, add: slider; add: level shaker;

--- Physics/Assets/Scripts/PlayerController.cs
+++ Physics/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject donutPrefab;
     public Material material;
+    public Rigidbody projectile;
+    public GameObject level;
+
+    public float slider = 0;
+    private bool counter = true;
 
     private void Start()
     {
@@ -17,24 +22,50 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public Rigidbody projectile;
-    public float projectileSpeed = 75;
-
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
+        {
+            sliderFunction();
+        }
+        if (Input.GetButtonUp("Fire1"))
         {
             Rigidbody clone;
             clone = Instantiate(projectile, new Vector3(-3.068f, -7.37f, -6.281f), transform.rotation) as Rigidbody;
-            clone.velocity = transform.TransformDirection(Vector3.up * projectileSpeed);
+            clone.AddForce(Vector3.up * slider, ForceMode.Impulse);
             StartCoroutine(destroyProjectile(clone));
+            slider = 0;
         }
-        if (Input.GetButtonDown("Fire2"))
+
+        if (Input.GetButton("Fire2"))
+        {
+            sliderFunction();
+        }
+        if (Input.GetButtonUp("Fire2"))
         {
             Rigidbody clone;
-            clone = Instantiate(projectile, new Vector3(3.09f, -7.37f, -6.281f), transform.rotation) as Rigidbody;
-            clone.velocity = transform.TransformDirection(Vector3.up * projectileSpeed);
+            clone = Instantiate(projectile, new Vector3(3.08f, -7.37f, -6.281f), transform.rotation) as Rigidbody;
+            clone.AddForce(Vector3.up * slider, ForceMode.Impulse);
             StartCoroutine(destroyProjectile(clone));
+            slider = 0;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(levelShaker(level, 0.1f));
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            StartCoroutine(levelShaker(level, -0.1f));
+        }
+    }
+
+    private IEnumerator levelShaker(GameObject level, float moveDistance)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            yield return null;
+            level.transform.Translate(moveDistance, 0, 0);
         }
     }
 
@@ -42,5 +73,25 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         Destroy(rb.gameObject);
+    }
+
+    private void sliderFunction()
+    {
+        if (slider <= 65 && counter)
+        {
+            slider += Time.deltaTime * 70;
+        }
+        else if (slider >= 65 && counter)
+        {
+            counter = false;
+        }
+        if (slider >= 65 && !counter)
+        {
+            slider -= Time.deltaTime * 70;
+        }
+        else if (slider <= 0 && !counter)
+        {
+            counter = true;
+        }
     }
 }
